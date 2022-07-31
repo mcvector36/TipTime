30cc3d6b54e84681b921dc8f7215e9a8ff2a415d
MC Vector 36 <constantinmihai804@gmail.com>
2022-07-31 22:52:12 +0300

    Git enabled

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tip Time
+The Tip Time app consists of one screen that shows the user a text input to enter a bill amount, radio buttons to select tip percentage, and a button to calculate the tip.
+
+I created a tip calculator app by building the layout first, then implement the logic to calculate the tip from the user input.
+
+https://developer.android.com/courses/pathways/android-basics-kotlin-unit-2-pathway-1
---
+++ app/src/main/java/com/mcvector36/tiptime/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.mcvector36.tiptime
+
+import android.content.Context
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import com.mcvector36.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initialize()
+
+    }
+
+    private fun initialize(){
+        binding.calculateButton.setOnClickListener{
+            calculateTip()
+        }
+        binding.costOfServiceEditText.setOnKeyListener {
+                view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
+
+    }
+
+    private fun calculateTip(){
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+
+        if(cost == null || cost == 0.0){
+            displayTip(0.0)
+            return
+        }
+
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = cost * tipPercentage
+
+        if(binding.roundUpSwitch.isChecked){
+            tip = kotlin.math.ceil(tip)
+        }
+
+        displayTip(tip)
+
+    }
+
+    private fun displayTip(tip: Double){
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+}
